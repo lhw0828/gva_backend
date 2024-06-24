d356be424324e141513873b0d91b5d2f20ed50c7
lhw <434962239@qq.com>
2024-06-25 00:53:16 +0800

    feat：video模块新增

--- router/bbs/xk_bbs_router.go
+++ router/bbs/xk_bbs_router.go
@@ -10,7 +10,7 @@ type XkBbsRouter struct{}
 func (e *XkBbsRouter) InitXkBbsRouter(Router *gin.RouterGroup) {
 
 	xkBbsRouterWithoutRecord := Router.Group("bbs")
-	xkBbsApi := v1.ApiGroupApp.BbsApiGroup
+	xkBbsApi := v1.ApiGroupApp.BbsApiGroup.XkBbsApi
 	{
 		//对外暴露的接口 http://localhost:8888/bbs/get?id=123
 		xkBbsRouterWithoutRecord.GET("get", xkBbsApi.GetXkBbs)                 // 获取单一客户信息
